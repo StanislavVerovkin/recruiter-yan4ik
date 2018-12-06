76f861475c6b614b2bcaeb14a6050d88b72d55cb
Stanislav Verovkin <sverevkin25@gmail.com>
2018-12-07 00:06:09 +0200

    currency

--- src/app/services/candidates.service.ts
+++ src/app/services/candidates.service.ts
@@ -6,14 +6,16 @@ import {Observable} from 'rxjs';
 @Injectable()
 export class CandidatesService {
 
-  constructor(private http: HttpClient) {
-  }
+    public apiUrl = 'https://recruiter-yanchik.firebaseio.com/';
 
-  addCandidate(candidate: Candidate): Observable<any> {
-    return this.http.post('https://recruiter-yanchik.firebaseio.com/candidates.json', candidate);
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  getCandidates(): Observable<any> {
-    return this.http.get('https://recruiter-yanchik.firebaseio.com/candidates.json');
-  }
+    addCandidate(candidate: Candidate): Observable<any> {
+        return this.http.post(`${this.apiUrl}candidates.json`, candidate);
+    }
+
+    getCandidates(): Observable<any> {
+        return this.http.get(`${this.apiUrl}candidates.json`);
+    }
 }
