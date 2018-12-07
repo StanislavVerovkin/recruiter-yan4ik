a4f56293eac40f26e2c331db6e160aeb9c5e7481
sverevkin25@gmail.com <82106610Vereva>
2018-12-07 17:14:26 +0200

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {SharedModule} from './shared/shared.module';
-import {CandidatesService} from './main/shared/services/candidates.service';
-import {CurrencyService} from './main/shared/services/currency.service';
 import {AuthModule} from './auth/auth.module';
 import {MainModule} from './main/main.module';
 
@@ -25,7 +23,7 @@ import {MainModule} from './main/main.module';
         BrowserAnimationsModule,
     ],
     bootstrap: [AppComponent],
-    providers: [CandidatesService, CurrencyService]
+    providers: []
 })
 export class AppModule {
 }
